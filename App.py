b8e1ee5a6940b4cb5e412f2361ddaafffedd7d09
IvanLink-py <70721607+IvanLink-py@users.noreply.github.com>
2022-01-12 15:31:27 +0500

    Delete App.py

--- App.py
+++
@@ -1,9 +0,0 @@
-from VideoSetter import VideoSetter
-
-class App:
-    def __init__(self):
-        self._video = VideoSetter('Experiments/E-Test/video.mp4')
-
-    def run(self):
-        self._video.set()
-        self._video.scan()
